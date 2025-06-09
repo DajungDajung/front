b38b02a6e4ae54ae8f1a62555edaf33cc09d5585
jiseong1688 <wltjd1688@naver.com>
2025-06-09 13:38:43 +0900

    fix: lightningcss 네이트비 바이너리를 못찾아서 vite설정으로  ESBuild 사용 강제

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,4 @@
-import {defineConfig} from 'vite';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 
@@ -6,4 +6,5 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   base: '/',
+  build: { minify: 'esbuild', cssMinify: 'esbuild' },
 });
